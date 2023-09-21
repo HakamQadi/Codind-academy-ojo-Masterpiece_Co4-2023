772416b103b9cfa39e303a16d8b0c48a21704569
HakamQadi <hakam.h.qadi@gmail.com>
2023-09-21 10:23:16 +0300

    edit

--- Back-End/routes/AdminRoutes.js
+++ Back-End/routes/AdminRoutes.js
@@ -6,6 +6,6 @@ router.route("/").get(adminController.getAllUsersByRole);
 router.route("/add_user").post(adminController.addUser);
 router.route("/delete_user/:id").delete(adminController.deleteUser);
 router.route("/edit_user/:id").patch(adminController.updateUser);
-router.route("/login").patch(adminController.login);
+router.route("/login").post(adminController.login);
 
 module.exports = router;
