e0782f35546307102c0da3dd370062c122ddeab3
HakamQadi <hakam.h.qadi@gmail.com>
2023-09-21 12:14:00 +0300

    !

--- Back-End/controller/AdminController.js
+++ Back-End/controller/AdminController.js
@@ -1,4 +1,4 @@
-const User = require("../model/UserModel");
+const {User} = require("../model/UserModel");
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
