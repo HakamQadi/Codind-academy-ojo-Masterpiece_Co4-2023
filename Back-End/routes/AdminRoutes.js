fbdb3a980dbcedfbe94993e8debee59e64a34b80
HakamQadi <hakam.h.qadi@gmail.com>
2023-09-21 10:22:39 +0300

    edit

--- Back-End/routes/AdminRoutes.js
+++ Back-End/routes/AdminRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const adminController = require('../controller/AdminController')
+const express = require("express");
+const router = express.Router();
+const adminController = require("../controller/AdminController");
 
-router.route('/').get(adminController.getAllUsersByRole)
-router.route('/add_user').post(adminController.addUser)
-router.route('/delete_user/:id').delete(adminController.deleteUser)
-router.route('/edit_user/:id').patch(adminController.updateUser)
+router.route("/").get(adminController.getAllUsersByRole);
+router.route("/add_user").post(adminController.addUser);
+router.route("/delete_user/:id").delete(adminController.deleteUser);
+router.route("/edit_user/:id").patch(adminController.updateUser);
+router.route("/login").patch(adminController.login);
 
-module.exports = router
+module.exports = router;
